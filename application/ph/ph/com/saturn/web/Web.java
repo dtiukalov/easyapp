01164aa24b21ed1f666bff1b9d919ddb49069598
guyuzmq <guyuzmq@gmail.com>
2012-02-11 14:27:11 +0000

    LO

--- application/ph/ph/com/saturn/web/Web.java
+++ application/ph/ph/com/saturn/web/Web.java
@@ -1,8 +1,97 @@
 package com.saturn.web;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import sun.net.www.content.text.plain;
+
 public class Web {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String compareByKW(List<String> list) {
+		if (list == null) {
+			return "[]";
+		}
+		
+		if (list != null && list instanceof List) {
+			List<Integer> newList = new ArrayList<Integer>();
+			
+			for (int i = 1; i < list.size(); i++) {
+				newList.add(Integer.parseInt(list.get(i)) - Integer.parseInt(list.get(i-1)));
+			}
+			
+			return ((List)newList).toString();
+		}
+		
+		return list + "";
+	}
+	
+	public static List<String> SumList(List<String> green, 
+			List<String> yellow, List<String> red) {
+		
+		if (green == null || yellow == null || red == null) {
+			return null;
+		}
+		
+		if (green != null && yellow != null && red != null){
+			List<String> sumList = new ArrayList<String>();
+			
+			for(int i=0; i<green.size(); i++) {
+				String sum = Integer.parseInt(green.get(i)) + 
+				Integer.parseInt(yellow.get(i)) +
+				Integer.parseInt(red.get(i)) + "";
+				sumList.add(sum);
+			}
+			return sumList;
+		}
+			
+		return null;
+	}
+	
+	public static String getSumNumberListStr(Object list) {
+		if (list == null) {
+			return "[]";
+		}
+		
+		if (list != null && list instanceof List) {
+			List<Integer> newList = new ArrayList<Integer>();
+			int sum = 0;
+			for (int i=0; i<((ArrayList)list).size(); i++){
+				sum += Integer.parseInt((String)((ArrayList)list).get(i));
+				newList.add(sum);
+			}
+			return ((List)newList).toString();
+		}
+		
+		return list + "";
+	}
+	
+	public static String getStrFromIntArray(int[] list) {
+		String str = "[";
+		if (list.length > 0) {
+			for (int i = 0; i < list.length-1; i++) {
+				str += list[i] + ",";
+			}
+			str += list[list.length-1];
+		}
+		str += "]";
+		return str;
+	}
+	
+	public static String getStrFromStringArray(String[] list) {
+		String str = "[";
+		if (list.length > 0) {
+			for (int i = 0; i < list.length-1; i++) {
+				str += "\"" + list[i] + "\", ";
+			}
+			str += "\"" + list[list.length-1] + "\"";
+		}
+		str += "]";
+		return str;
+	}
 	
 	public static String getNumberListStr(Object list) {
 		if (list == null) {
@@ -10,6 +99,19 @@ public class Web {
 		}
 		
 		if (list instanceof List) {
+			return ((List)list).toString();
+		}
+		
+		return list + "";
+	}
+	
+	public static String getNumberListStrSubFirst(Object list) {
+		if (list == null) {
+			return "[]";
+		}
+		
+		if (list instanceof List) {
+			list = ((List) list).subList(1, ((List) list).size());
 			return ((List)list).toString();
 		}
 		

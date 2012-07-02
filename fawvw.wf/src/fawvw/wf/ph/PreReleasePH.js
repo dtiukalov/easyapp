11d7ed0ed131aaee0d16111e14e0e4d9fd0fa157
guyuzmq@gmail.com <guyuzmq@gmail.com@c84a3c69-0e63-e6b3-e08f-32a2bf6b3039>
2012-07-02 14:54:11 +0000

    刘乐：因为关系存在伪文件夹，所以获取*的关系无法获取数据，要写具体的关系名

--- fawvw.wf/src/fawvw/wf/ph/PreReleasePH.js
+++ fawvw.wf/src/fawvw/wf/ph/PreReleasePH.js
@@ -4,8 +4,25 @@ function fawvwPreleasePH(task) {
     new TopoSetProperties({
 
 $path:[
-  "root_target_attachments:FV9PHReportRevision/*:Form",
-  "root_target_attachments:FV9PHReportRevision/*:Dataset"
+  "root_target_attachments:FV9PHReportRevision/FV9LOPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9LOPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9PMPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9PMPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9CP1PH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9CP1PH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9VSCPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9VSCPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9PLPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9PLPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9TEPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9TEPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9SUPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9SUPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9QAPH_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9QAPH_Rel:Dataset",
+  "root_target_attachments:FV9PHReportRevision/FV9PHBackup_Rel:Form",
+  "root_target_attachments:FV9PHReportRevision/FV9PHBackup_Rel:Dataset"
+
 ],
 $match:[ [
     ["/owning_user","/@user"]
@@ -14,7 +31,7 @@ $match:[ [
     ["一汽大众","/owning_group/parent/parent.name"]
   ]
 ],
-fv9PreRelesed: "true"
+fv9PreRelesed: "Yes"
 
     }).process(task);
   } catch (e) {
@@ -24,5 +41,4 @@ fv9PreRelesed: "true"
   }
 
 }
-
 var 一汽大众预发布PH报告 = fawvwPreleasePH;

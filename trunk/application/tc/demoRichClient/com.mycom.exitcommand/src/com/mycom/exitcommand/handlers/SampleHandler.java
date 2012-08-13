59a2265331d064b1cf5ece7fdc2842f68af8aa6d
guyuzmq@gmail.com <guyuzmq@gmail.com@c84a3c69-0e63-e6b3-e08f-32a2bf6b3039>
2012-08-13 06:25:06 +0000

    初始导入。

---
+++ trunk/application/tc/demoRichClient/com.mycom.exitcommand/src/com/mycom/exitcommand/handlers/SampleHandler.java
@@ -0,0 +1,29 @@
+package com.mycom.exitcommand.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class SampleHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public SampleHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		HandlerUtil.getActiveWorkbenchWindow(event).close();
+		return null;
+	}
+}

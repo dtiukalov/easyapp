d33426be19bf70b1d6379eaf5793ef66db173c6b
guyuzmq@gmail.com <guyuzmq@gmail.com@c84a3c69-0e63-e6b3-e08f-32a2bf6b3039>
2012-10-22 06:41:28 +0000

    pos传错了

--- trunk/application/application/language/chinese/error_lang.php
+++
@@ -1,4 +0,0 @@
-<?php
-$lang['error_no_permission_module']='您没有权限访问的这个模块';
-$lang['error_unknown']='未知';
-?>
--- trunk/application/application/language/chinese/login_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lang['login_login']='登录';
-$lang['login_username']='用户名';
-$lang['login_password']='密码';
-$lang['login_go']='登录';
-$lang['login_invalid_username_and_password']='无效的用户名或密码';
-$lang['login_welcome_message']='欢迎来到零售pos站。若要继续，请使用您的用户名和密码登录。';
-?>

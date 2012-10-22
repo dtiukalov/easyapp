468f85b5f1de085a48ab43580825b8d57775586c
guyuzmq <guyuzmq@gmail.com>
2012-10-22 06:43:41 +0000

    正阳轮胎店的收银库存程序

---
+++ application/pos/application/language/chinese/config_lang.php
@@ -0,0 +1,24 @@
+<?php
+$lang['config_info']='存储配置信息';
+$lang['config_company']='公司名称';
+$lang['config_address']='公司地址';
+$lang['config_phone']='公司电话';
+$lang['config_website']='网址';
+$lang['config_fax']='传真';
+$lang['config_default_tax_rate']='默认税率 %';
+$lang['config_default_tax_rate_1']='税率1';
+$lang['config_default_tax_rate_2']='税率2';
+$lang['config_company_required']='公司名称是必填字段';
+$lang['config_address_required']='公司地址是必填字段';
+$lang['config_phone_required']='公司电话是必填字段';
+$lang['config_default_tax_rate_required']='默认的税率是必填字段';
+$lang['config_default_tax_rate_number']='默认的税率必须是一个数字';
+$lang['config_company_website_url']='公司的网站不是一个有效的URL(http://...)';
+$lang['config_saved_successfully']='配置保存成功';
+$lang['config_saved_unsuccessfully']='配置保存不成功';
+$lang['config_return_policy_required']='退货政策是必填字段';
+$lang['config_print_after_sale']='出售后打印收据';
+$lang['config_language'] = '语言';
+$lang['config_timezone'] = '时区';
+$lang['config_currency_symbol'] = '货币符号';
+?>

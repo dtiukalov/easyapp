7bcaca7aedda13ea99e9eb680b68f25e011120a1
guyuzmq <guyuzmq@gmail.com>
2012-12-09 08:33:56 +0000

    刘乐：添加Agenda jsp

--- application/tc/fawvw/ph/ph/com/saturn/ph/form/AgendaForm.java
+++ application/tc/fawvw/ph/ph/com/saturn/ph/form/AgendaForm.java
@@ -6,7 +6,18 @@ public class AgendaForm extends Form {
 	
 	private static final String[] attr = {
 		"fv9PreRelesed", "fv9Oraganization", "release_status_list", "fv9PageName",
-		"fv9PlatformType","fv9otherUid", "fv9IsBackup", "object_type","fv9SortNum"
+		"fv9PlatformType", "fv9otherUid", "fv9IsBackup", "object_type",
+		"fv9DisplayRule","fv9SortNum",
+		
+		"fv9ThemaNo",
+		"fv9Thema",
+		"fv9Vortragender",
+		"fv9InBF",
+		"fv9InLF",
+		"fv9InVFF",
+		"fv9InPVS",
+		"fv9In0S",
+		"fv9InSOP"
 	};
 	
 	public String[] getAttributes() {
@@ -14,12 +25,12 @@ public class AgendaForm extends Form {
 	}
 
 	public String getJspPath() {
-		return "/app/pep/imageDataset.jsp";
+		return "/app/pep/agenda.jsp";
 	}
 
 	@Override
 	public String getType() {
-		return "Agenda";
+		return "FV9_Agenda";
 	}
 
 	@Override
